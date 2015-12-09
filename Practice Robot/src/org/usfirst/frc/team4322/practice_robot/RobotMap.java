59207f7ac15a85531eadbbe59a7302b6d1b5de41
nathanbaugh <nathanbaugh@yahoo.com>
2015-12-09 12:34:28 -0800

    Initial Commit Added RobotMap

---
+++ Practice Robot/src/org/usfirst/frc/team4322/practice_robot/RobotMap.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team4322.practice_robot;
+
+import java.io.IOException;
+import java.util.jar.JarFile;
+
+public class RobotMap {
+	
+	// Create a String with the Last Build Time of Code
+	public static String LAST_BUILD_TIME;
+	static{
+		try {
+			LAST_BUILD_TIME = new JarFile(RobotMap.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getManifest().getMainAttributes().getValue("Build-Time");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// Set Joystick Ports for Drive Station
+	public static int PILOT = 0;
+	public static int COPILOT = 1;
+	
+	// Set CAN_Jaguar Ports
+	public static int CAN_Jaguar_1 = 10;
+	public static int CAN_Jaguar_2 = 11;
+
+}
